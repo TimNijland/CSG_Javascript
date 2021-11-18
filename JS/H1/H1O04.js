4aeefcc07673980837299f5145233a99bba7fefe
TimNijland <153286@csgaugustinus.nl>
2021-11-18 09:56:26 +0000

    huts

--- JS/H1/H1O04.js
+++ JS/H1/H1O04.js
@@ -6,29 +6,29 @@ function setup() {
 }
 
 function draw() {
-  // teken de grond
   noStroke();
   fill('wheat');
   rect(0,350,450,100);
-  // teken het huis
+
   strokeWeight(4);
   stroke('darkgrey');
   fill('lightgray');
   rect(100,280,100,100);
   fill('gray');
-  // VERVANG DEZE REGEL door een regel die het dak tekent
-  // teken de deur
+  
+  strokeWeight(4);
+  stroke('darkgrey')
+  triangle(150,200,100,280,200,280)
+
   noStroke();
-  // PAS ONDERSTAANDE REGEL AAN zodat de deur op de juiste plaats staat
-  rect(10,10,30,50);
-  // teken de maan
+  rect(120,330,30,50);
+
+  noStroke()
   fill('khaki');
-  // VERVANG DEZE REGEL door een regel die de maan tekent met een diameter van 150 en 25 vanaf de rand
-  // teken de boom
+  ellipse(350,100,150)
+
   fill('sienna');
   rect(300,250,40,130);
   fill('olive');
-  // VERVANG DEZE REGEL door een regel die het groen van de boom tekent met behulp van ellipse
-  // Als je een cirkel wilt tekenen met diameter 300 kun je ellipse(0,0,300) gebruiken.
-  // Als je een ellipse wilt maken met breedte 300 en hoogte 500, dan moet je een parameter toevoegen: ellipse(0,0,300,500).
+  ellipse(320,230,100,150)
 }
